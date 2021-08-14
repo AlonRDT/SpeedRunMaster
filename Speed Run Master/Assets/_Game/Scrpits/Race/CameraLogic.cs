862a164eb6c1bb53c8795c8430237f290e9a05fe
AlonRDT <55650759+AlonRDT@users.noreply.github.com>
2021-08-14 12:01:19 +0300

    Camera Rig implemented

--- Speed Run Master/Assets/_Game/Scrpits/Race/CameraLogic.cs
+++ Speed Run Master/Assets/_Game/Scrpits/Race/CameraLogic.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraLogic : MonoBehaviour
 {
+    [SerializeField] private float m_ReturnCameraToDefaultPositionSpeed;
+    [SerializeField] private float m_MoveCameraSpeed;
+    [SerializeField] private GameObject m_YRotationBase;
+    private GameInput m_Input;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Input = new GameInput();
+        m_Input.Gameplay.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 cameraMovementVector = m_Input.Gameplay.TurnCamera.ReadValue<Vector2>();
+
+        if (cameraMovementVector.x == 0 && cameraMovementVector.y == 0)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.identity, m_ReturnCameraToDefaultPositionSpeed * Time.fixedDeltaTime);
+            m_YRotationBase.transform.localRotation = Quaternion.Lerp(m_YRotationBase.transform.localRotation, Quaternion.identity, m_ReturnCameraToDefaultPositionSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            transform.Rotate(new Vector2(-cameraMovementVector.y, 0) * m_MoveCameraSpeed, Space.Self);
+            m_YRotationBase.transform.Rotate(new Vector2(0, cameraMovementVector.x) * m_MoveCameraSpeed, Space.Self);
+        }
     }
 }
