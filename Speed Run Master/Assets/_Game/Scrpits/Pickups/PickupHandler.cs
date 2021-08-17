de8604db2327867de51c9f038b0d73a67505a514
AlonRDT <55650759+AlonRDT@users.noreply.github.com>
2021-08-17 18:50:39 +0300

    Should work now

--- Speed Run Master/Assets/_Game/Scrpits/Pickups/PickupHandler.cs
+++ Speed Run Master/Assets/_Game/Scrpits/Pickups/PickupHandler.cs
@@ -12,10 +12,12 @@ public class PickupHandler : MonoBehaviour
     [SerializeField] private Image m_PickupImage;
     private int m_CurrentPickupIndex;
     private bool m_ActionInProgress;
+    private AudioSource m_NoPickupAudio;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_NoPickupAudio = GetComponent<AudioSource>();
         m_PickupImage.sprite = m_PickupSprites[0];
         m_CurrentPickupIndex = -1;
         m_ActionInProgress = false;
@@ -94,6 +96,10 @@ public class PickupHandler : MonoBehaviour
         {
             m_Weapons[newIndex].SelectPickup();
         }
+        else
+        {
+            m_NoPickupAudio.Play();
+        }
 
         m_PickupImage.sprite = m_PickupSprites[newIndex + 1];
         m_CurrentPickupIndex = newIndex;
@@ -105,6 +111,10 @@ public class PickupHandler : MonoBehaviour
         {
             m_Weapons[m_CurrentPickupIndex].ActivatePickup();
             m_ActionInProgress = true;
+        }
+        else if(m_CurrentPickupIndex == -1 && context.phase == InputActionPhase.Started)
+        {
+            m_NoPickupAudio.Play();
         }
         else if (m_CurrentPickupIndex != -1 && context.phase == InputActionPhase.Canceled)
         {
